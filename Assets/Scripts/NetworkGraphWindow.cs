833d6663ba82ac18bc0e43a3c27232309fd3f605
Michael Dorst <m@mdorst.net>
2020-11-23 02:16:55 -0800

    Refactor node generation

--- Assets/Scripts/NetworkGraphWindow.cs
+++ Assets/Scripts/NetworkGraphWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class NetworkGraphWindow : MonoBehaviour
 {
     [SerializeField] private Sprite nodeSprite;
     [SerializeField] private float nodeSize;
+    [SerializeField] private float edgeWidth;
     private RectTransform _graphContainer;
     private void Awake()
     {
@@ -14,37 +16,77 @@ public class NetworkGraphWindow : MonoBehaviour
         CreateNetwork(new List<int> {5, 10, 5});
     }
 
-    private void CreateNode(Vector2 anchoredPosition)
+    private List<Vector2> GetNodePositions(List<int> layerSizes, float viewWidth, float viewHeight)
+    {
+        var positions = new List<Vector2>();
+        var xInterval = viewWidth / (layerSizes.Count + 1);
+        for (var i = 0; i < layerSizes.Count; i++)
+        {
+            var yInterval = viewHeight / (layerSizes[i] + 1);
+            for (var j = 0; j < layerSizes[i]; j++)
+            {
+                positions.Add(new Vector2((i + 1) * xInterval, (j + 1) * yInterval));
+            }
+        }
+        return positions;
+    }
+
+    private GameObject CreateNode(Vector2 position)
     {
         var node = new GameObject("Node", typeof(Image));
         node.transform.SetParent(_graphContainer, false);
         node.GetComponent<Image>().sprite = nodeSprite;
         var rectTransform = node.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.anchoredPosition = position;
         rectTransform.sizeDelta = new Vector2(nodeSize, nodeSize);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, y: 0);
+        return node;
     }
 
-    private void CreateLayer(int nodeCount, float x)
+    private void CreateNodes(List<Vector2> nodePositions)
     {
-        var rectTransform = GetComponent<RectTransform>();
-        var viewHeight = rectTransform.rect.height;
-        var interval = viewHeight / (nodeCount + 1f);
-        for (var i = 1; i <= nodeCount; i++)
+        foreach (var nodePosition in nodePositions)
         {
-            CreateNode(new Vector2(x, i * interval));
+            CreateNode(nodePosition);
         }
     }
 
-    private void CreateNetwork(List<int> layers)
+    private void CreateEdge(Vector2 firstVertex, Vector2 secondVertex)
     {
-        var rectTransform = GetComponent<RectTransform>();
-        var viewWidth = rectTransform.rect.width;
-        var interval = viewWidth / (layers.Count + 1f);
-        for (var i = 0; i < layers.Count; i++)
+        var edge = new GameObject("Edge", typeof(Image));
+        edge.transform.SetParent(_graphContainer, false);
+        var rectTransform = edge.GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(100, edgeWidth);
+        rectTransform.anchoredPosition = firstVertex;
+    }
+
+    private void CreateEdges(List<Vector2> nodePositions, List<int> layerSizes)
+    {
+        var startingIndex = 0;
+        for (var i = 0; i < layerSizes.Count - 1; i++)
         {
-            CreateLayer(layers[i], (i + 1) * interval);
+            var leftLayerSize = layerSizes[i];
+            var rightLayerSize = layerSizes[i + 1];
+            for (var j = 0; j < leftLayerSize; j++)
+            {
+                for (var k = 0; k < rightLayerSize; k++)
+                {
+                    CreateEdge(nodePositions[startingIndex + j], nodePositions[startingIndex + leftLayerSize + k]);
+                }
+            }
+
+            startingIndex += leftLayerSize;
         }
+    }
+
+    private void CreateNetwork(List<int> layerSizes)
+    {
+        var rect = GetComponent<RectTransform>().rect;
+        var nodePositions = GetNodePositions(layerSizes, rect.width, rect.height);
+        CreateNodes(nodePositions);
+        CreateEdges(nodePositions, layerSizes);
     }
 }
